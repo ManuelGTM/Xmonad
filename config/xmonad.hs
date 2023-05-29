3fba1629594364a9fa60de73656f2704ed29bdeb
ManuelGTM <manuelgabrielt@gmail.com>
2023-05-29 00:37:31 +0000

    Spacing windows

--- config/xmonad.hs
+++ config/xmonad.hs
@@ -4,7 +4,9 @@ import XMonad
 import Data.Monoid
 import System.Exit
 import XMonad.Util.SpawnOnce
-
+import XMonad.Util.Run
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spacing
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
@@ -176,8 +178,10 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 --
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
+--  
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+  
+myLayout = spacingWithEdge 6 $ avoidStruts (tiled ||| Mirror tiled ||| Full)
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -240,16 +244,18 @@ myLogHook = return ()
 --
 -- By default, do nothing.
 myStartupHook = do
-
-	spawnOnce "nitrogen --restore &"
-	spawnOnce "picom --experimental-backend &"
+        spawnOnce "nitrogen --restore &"
+        spawnOnce "picom --experimental-backend &"
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = xmonad defaults
+main = do
+  xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc"
+  xmonad $ docks defaults
+ 
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
