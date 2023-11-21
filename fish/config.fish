f6462071bb50f1c2c90f434e3e9cd773cc7ae329
ManuelGTM <manuelgabrielt@gmail.com>
2023-11-21 17:14:32 +0000

    Fish and alacritty upgraded rice

--- fish/config.fish
+++ fish/config.fish
@@ -6,24 +6,26 @@ if status is-interactive
     # Sashimi prompt
     function fish_prompt
       set -l last_status $status
-      set -l cyan (set_color -o cyan)
-      set -l yellow (set_color -o yellow)
-      set -g red (set_color -o red)
-      set -g blue (set_color -o blue)
-      set -l green (set_color -o green)
-      set -g normal (set_color normal)
+      set -l cyan (set_color -o 7DF9FF)
+      set -l yellow (set_color -o e0af68)
+      set -l Orange (set_color -o ff9e64)
+      set -g red (set_color -o f7768e)
+      set -g blue (set_color -o 008080)
+      set -l green (set_color -o 9ece6a)
+      set -l purple (set_color -o 9d7cd8)
+      set -g normal (set_color -o C0C0C0) 
 
       set -l ahead (_git_ahead)
       set -g whitespace ' '
 
       if test $last_status = 0
         set initial_indicator "$green◆"
-        set status_indicator "$normal❯$cyan❯$green❯"
+        set status_indicator "$normal❯$cyan❯$yellow❯"
       else
         set initial_indicator "$red✖ $last_status"
         set status_indicator "$red❯$red❯$red❯"
       end
-      set -l cwd $cyan(basename (prompt_pwd))
+      set -l cwd $Orange(basename (prompt_pwd))
 
       if [ (_git_branch_name) ]
 
@@ -84,7 +86,46 @@ if status is-interactive
         fish_vi_key_bindings
     end
     #end vim keybindings
- 
+    
+   # TokyoNight Color Palette
+    set -l foreground c0caf5
+    set -l selection 283457
+    set -l comment 565f89
+    set -l red f7768e
+    set -l orange ff9e64
+    set -l yellow e0af68
+    set -l green 9ece6a
+    set -l purple 9d7cd8
+    set -l cyan 7dcfff
+    set -l pink bb9af7
+
+    # Syntax Highlighting Colors
+    set -g fish_color_normal $foreground
+    set -g fish_color_command $cyan
+    set -g fish_color_keyword $pink
+    set -g fish_color_quote $yellow
+    set -g fish_color_redirection $green
+    set -g fish_color_end $orange
+    set -g fish_color_error $red
+    set -g fish_color_param $green
+    set -g fish_color_comment $comment
+    set -g fish_color_selection --background=$selection
+    set -g fish_color_search_match --background=$selection
+    set -g fish_color_operator $purple
+    set -g fish_color_valid_path $green
+    set -g fish_color_escape $pink
+    set -g fish_color_autosuggestion $comment
+    set -g fish_color_cancel $yellow
+
+
+    # Completion Pager Colors
+    set -g fish_pager_color_progress $comment
+    set -g fish_pager_color_prefix $cyan
+    set -g fish_pager_color_completion $foreground
+    set -g fish_pager_color_description $comment
+    set -g fish_pager_color_selected_background --background=$selection
+
+    #
 
     # Activates Neofetch
     neofetch
