d65936e85a39cb76aa803a6614813caca1b14fba
manuelGTM <manuelgabrielt@gmail.com>
2024-02-11 16:36:25 -0400

    Catppuccin Lualine

--- nvim/lua/manuel/plugins/lualine.lua
+++ nvim/lua/manuel/plugins/lualine.lua
@@ -6,14 +6,15 @@ return {
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
 		local colors = {
-			blue = "#89B4FA",
-			green = "#A6E3A1",
-			violet = "#F5C2E7",
-			yellow = "#F9E2AF",
-			red = "#F38BA8",
-			fg = "#CDD6F4",
-			bg = "#1E1E2E",
-			inactive_bg = "#45475A",
+			blue = "#89bceb",
+			green = "#a6e3a1",
+			violet = "#cba6f7",
+			yellow = "#fab387",
+			red = "#f38ba8",
+			surf1 = "#45475a",
+			fg = "#bac2de",
+			bg = "#313244",
+			inactive_bg = "#2c3043",
 		}
 
 		local my_lualine_theme = {
@@ -21,31 +22,37 @@ return {
 				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
+				y = { bg = colors.surf1, fg = colors.blue, gui = "bold" },
 			},
 			insert = {
 				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
+				y = { bg = colors.surf1, fg = colors.green, gui = "bold" },
 			},
 			visual = {
 				a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
+				y = { bg = colors.surf1, fg = colors.violet, gui = "bold" },
 			},
 			command = {
 				a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
+				y = { bg = colors.surf1, fg = colors.yellow, gui = "bold" },
 			},
 			replace = {
 				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
 				b = { bg = colors.bg, fg = colors.fg },
 				c = { bg = colors.bg, fg = colors.fg },
+				y = { bg = colors.surf1, fg = colors.red, gui = "bold" },
 			},
 			inactive = {
 				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
 				b = { bg = colors.inactive_bg, fg = colors.semilightgray },
 				c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+				y = { bg = colors.surf1, fg = colors.inactive_bg, gui = "bold" },
 			},
 		}
 
@@ -53,7 +60,6 @@ return {
 		lualine.setup({
 			options = {
 				theme = my_lualine_theme,
-				-- theme = "catppuccin",
 			},
 			sections = {
 				lualine_x = {
